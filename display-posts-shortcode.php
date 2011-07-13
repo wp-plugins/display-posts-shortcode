e4cf03309b5e6e6579f231b7df1525f5df3c4a85
billerickson <billerickson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-13 20:00:07 +0000

    Initial upload
    git-svn-id: https://plugins.svn.wordpress.org/display-posts-shortcode/trunk@409444 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display-posts-shortcode.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Plugin Name: Display Posts Shortcode
+ * Plugin URI: http://www.billerickson.net/shortcode-to-display-posts/
+ * Description: Display a listing of posts using the [display-posts] shortcode
+ * Version: 0.1
+ * Author: Bill Erickson
+ * Author URI: http://www.billerickson.net
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume 
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @package Display Posts
+ * @version 0.1
+ * @author Bill Erickson <bill@billerickson.net>
+ * @copyright Copyright (c) 2011, Bill Erickson
+ * @link http://www.billerickson.net/shortcode-to-display-posts/
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+ 
+// Create the shortcode
+add_shortcode('display-posts', 'be_display_posts_shortcode');
+function be_display_posts_shortcode($atts) {
+    extract(shortcode_atts(array(
+        'tag' => '',
+        'category' => '',
+        'posts_per_page' => '10',
+        'include_date' => '',
+        'order' => 'DESC',
+        'orderby' => 'date',
+    ), $atts));
+ 
+    $args = array(
+        'tag' => $tag,
+        'category_name' => $category,
+        'posts_per_page' => $posts_per_page,
+        'order' => $order,
+        'orderby' => $orderby,
+    );
+ 
+    $return = '';
+    $listing = new WP_Query($args);
+    if ($listing->have_posts()):
+        $return .= '<ul>';
+ 
+        while ($listing->have_posts()): $listing->the_post(); global $post;
+            $return .= '<li><a href="'.get_permalink().'">'.get_the_title().'</a>';
+            if ($include_date) $return .= ' ('.get_the_date('n/j/Y').')';
+            $return .= '</li>';
+        endwhile;
+ 
+        $return .= '</ul>';
+    endif; wp_reset_query();
+ 
+    if (!empty($return)) return $return;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Display Posts Shortcode ===
+Contributors: billerickson
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=AEDKVR5B4TDWN
+Tags: shortcode, pages, posts, page, query, display, list
+Requires at least: 3.0
+Tested up to: 3.2.1
+Stable tag: 0.1
+
+Display a listing of posts using the [display-posts] shortcode
+
+== Description ==
+
+The *Display Posts Shortcode* was written to allow users to easily display listings of posts without knowing PHP or editing template files.
+
+Add the shortcode in a post or page, and use the arguments to query based on tag and/or category, limit the number of posts displayed, and sort the results. I’ve also added an extra condition, include_date, which will place the date at the end of the post.
+
+See the [WordPress Codex](http://codex.wordpress.org/Class_Reference/WP_Query) for information on using the arguments.
+
+=Examples=
+
+[listing tag="advanced" posts_per_page="20"]
+This will list the 20 most recent posts with the tag “Advanced”.
+
+[listing category="must-read" posts_per_page="-1" include_date="true" order="ASC" orderby="title"]
+This will list every post in the Must Read category, in alphabetical order, with the date appended to the end.
+
+=Arguments=
+* tag
+* category
+* posts_per_page
+* order
+* orderby
+* include_date
+
+
+== Installation ==
+
+1. Upload `display-posts-shortcode` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the *Plugins* menu in WordPress.
+1. Add the shortcode to a post or page. 
+
+
+== Changelog ==
+
+**Version 0.1**
+
+* This is version 0.1.  Everything's new!
+
